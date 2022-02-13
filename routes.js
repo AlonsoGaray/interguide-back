2623dff5372f7b5578f796e0b2f8d886151ff478
AlonsoGaray <alonsogarayp@gmail.com>
2022-02-13 11:25:51 -0500

    Maqueteado inicial de tags y questions

--- routes.js
+++ routes.js
@@ -1,10 +1,14 @@
 const user = require('./api/user');
 const authLocal = require('./auth/local');
 const upload = require('./api/upload');
+const tag = require('./api/tag');
+const question = require('./api/question');
 
 function routes(app) {
   app.use('/api/users', user);
   app.use('/api/uploads', upload);
+  app.use('/api/tags', tag);
+  app.use('/api/questions', question);
 
   // auth routes
   app.use('/auth/local', authLocal);
